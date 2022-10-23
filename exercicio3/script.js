6a68c08027303da4418415d38018f0ccd4969d2c
fabiodelllima <f.f.fabiodelima@gmail.com>
2022-10-23 01:12:07 -0300

    Exercicio 3 finalizado

---
+++ exercicio3/script.js
@@ -0,0 +1,31 @@
+const A = 2 > 5 > 20
+const B = 5 === 5 || '5'
+const C = !20 > 50
+const D = !20 > 50 || 50 > 60
+
+const X = true
+const Y = false
+
+if (A) {
+  console.log(`A operação é ${X}`)
+} else {
+  console.log(`A operação é ${Y}`)
+}
+
+if (B) {
+  console.log(`A operação é ${X}`)
+} else {
+  console.log(`A operação é ${Y}`)
+}
+
+if (C) {
+  console.log(`A operação é ${X}`)
+} else {
+  console.log(`A operação é ${Y}`)
+}
+
+if (D) {
+  console.log(`A operação é ${X}`)
+} else {
+  console.log(`A operação é ${Y}`)
+}
